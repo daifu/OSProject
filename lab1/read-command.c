e878d5554374dfded989f63f20beea5c8df1fd4a
Tai Pham <taipham@ubuntu.(none)>
2012-04-04 14:12:57 -0700

    add function declaration and few functions to read-command.c

--- lab1/read-command.c
+++ lab1/read-command.c
@@ -2,8 +2,18 @@
 
 #include "command.h"
 #include "command-internals.h"
+#include "alloc.h"
 
 #include <error.h>
+
+
+/* function declaration */
+void init_command_stream(command_stream_t new_stream); // initialize an empty command stream
+int is_word(char c); // check if a character is a word (word definition is in the lab manual)
+int is_speacial_token(char); // check if a character is a special character (8 tokens in the lab manual)
+int is_comment(char); // check if char is #
+int is_white_space(char); // check if white space but NOT new line 
+/* end function declaration section */
 
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
@@ -22,4 +32,15 @@ read_command_stream (command_stream_t s)
   /* FIXME: Replace this with your implementation too.  */
   error (1, 0, "command reading not yet implemented");
   return 0;
+}
+
+void
+init_command_stream(command_stream_t new_stream)
+{
+  new_stream->head = (char*)checked_malloc(sizeof(char));
+  new_stream->line_num = 0;
+  new_stream->size = 0;
+  new_stream->next = NULL;
+  new_stream->prev = NULL;
+  
 }
